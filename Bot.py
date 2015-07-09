57e70114b8c11575554c7a95534d16a41d40e256
SLAPaper <slapaper.pku@gmail.com>
2015-07-09 22:56:06 +0800

    Debuging http POST method

--- Bot.py
+++ Bot.py
@@ -1,9 +1,16 @@
-import urllib, http, json, socketserver, ssl
+import urllib, http.client, http.server, json, socketserver, ssl
 
 TOKEN = "119827757:AAFTo0ezhROp-0Ria-zkjkGHfJeHtik8-Ow"
 PORT = 8443
-WEB_HOOK_HOST = "https://www.slapaper.cn:8443/" + TOKEN
-API = "https://api.telegram.org/"
-"bot" + TOKEN + "/"
+WEB_HOOK_HOST = "https://www.slapaper.cn:%d/" % PORT + TOKEN
+WEB_HOOK_API = "api.telegram.org"
+URL = "/bot" + TOKEN + "/"
 
-webhook_connection = http.client.HTTPConnection()
+webhook_connection = http.client.HTTPSConnection(WEB_HOOK_API, context=ssl.create_default_context())
+webhook_connection.connect()
+webhook_body = json.JSONEncoder().encode({"url":WEB_HOOK_HOST,})
+webhook_head = {"Content-Type":"application/x-www-form-urlencoded",}
+webhook_connection.request("POST", URL + "setWebhook", webhook_body, webhook_head)
+response = webhook_connection.getresponse()
+print(response.readlines())
+webhook_connection.close()
