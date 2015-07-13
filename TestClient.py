f267c12ac93c80f0cf9b9c1ea476f4b567589482
SLAPaper <slapaper.pku@gmail.com>
2015-07-13 21:03:06 +0800

    remove proxy from testClient

--- TestClient.py
+++ TestClient.py
@@ -11,8 +11,8 @@ PATH = dic["PATH"]
 WEB_HOOK_HOST = "https://" + PATH + TOKEN + "/"
 
 https_handler = urllib.request.HTTPSHandler(context=ssl.create_default_context())
-proxy_handler = urllib.request.ProxyHandler({'https':'127.0.0.1:8090',})
-opener = urllib.request.build_opener(https_handler, proxy_handler)
+# proxy_handler = urllib.request.ProxyHandler({'https':'127.0.0.1:8090',})
+opener = urllib.request.build_opener(https_handler)
 
 data = "Hello World!".encode('utf_8')
 response = opener.open(WEB_HOOK_HOST, data=data)
