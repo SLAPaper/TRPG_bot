1efcf3161614f6db6e49a009126da5a2f44656c6
SLAPaper <slapaper.pku@gmail.com>
2015-07-15 09:45:12 +0800

    write roll (without debug)

--- Update.py
+++ Update.py
@@ -23,15 +23,48 @@ def deal(Update):
 
 def roll(query):
 	# d = 1#1d20+0
-	# [count'#']'d'[max]['+'plus]' '[description]
-	if query == None:
+	# [times'#'][count]'d'[faces]['+'addend]' '[description]
+	
+	try:
+		if 'd' in query:
+			def f(s):
+				if s.isnumeric():
+					return int(s)
+				else:
+					return 1
+			front, back = query.split('d', maxsplit=2)
+			
+			if '#' in front:
+				times, count = map(f, front.split('#'))
+			else if front.isnumeric():
+				times = 1
+				count = int(front)
+			else:
+				times = 1
+				count = 1
+			
+			if ' ' in back:
+				back2, description = back.split(maxsplit=2)
+			else:
+				back2 = back
+				description = None
+			
+			if '+' in back2:
+				faces, addend = map(f, back2.split('+'))
+			else if: back2.isnumeric():
+				faces = int(back2)
+				addend = 0
+			else:
+				faces = 20
+				addend = 0
+		
+		result = []
+		for i in range(times):
+			dice_sum = 0
+			for j in range(count):
+				dice_sum += random.randrange(faces)
+			dice_sum += addend
+			result.append(dice_sum)
+		return tuple(result)
+	except:
 		return random.randrange(20) + 1
-	
-	if ' ' in query:
-		expr, description = query.split(maxsplit=2)
-	else:
-		expr = query
-		description = None
-	
-	result = 0
-	return result
