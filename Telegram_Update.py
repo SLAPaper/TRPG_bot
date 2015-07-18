cbc8ed06b190f2f18a2dbe916bc201651edacb76
SLAPaper <slapaper.pku@gmail.com>
2015-07-18 23:29:54 +0800

    split roll to bot_utlity

--- Telegram_Update.py
+++ Telegram_Update.py
@@ -1,4 +1,5 @@
 import random, Telegram_API
+from bot_utility import roll
 
 def deal(prefix, update):
     if update.message.text.startswith('/'):
@@ -21,51 +22,6 @@ def do_help(prefix, message):
     'me' [action]: do charactor action
     """
     Telegram_API.sendMessage(prefix, message.chat.id_, HELP_MESSAGE)
-
-def roll(query):
-    # d = 1#1d20+0
-    # [times'#'][count]'d'[faces]['+'addend]' '[description]
-    try:
-        def f(s):
-            if s.isnumeric():
-                return int(s)
-            else:
-                return 1
-        front, back = query.split('d', maxsplit=2)
-        
-        if '#' in front:
-            times, count = map(f, front.split('#'))
-        elif front.isnumeric():
-            times = 1
-            count = int(front)
-        else:
-            times = 1
-            count = 1
-        
-        if ' ' in back:
-            back2, description = back.split(maxsplit=2)
-        else:
-            back2 = back
-            description = None
-        
-        if '+' in back2:
-            faces, addend = map(f, back2.split('+'))
-        elif back2.isnumeric():
-            faces = int(back2)
-            addend = 0
-        else:
-            faces = 20
-            addend = 0
-        result = []
-        for i in range(times):
-            dice_sum = 0
-            for j in range(count):
-                dice_sum += random.randrange(faces)
-            dice_sum += addend
-            result.append(dice_sum)
-        return tuple(result)
-    except:
-        return (random.randrange(20) + 1,)
 
 def do_r(prefix, message):
     l = message.text.split(maxsplit=3)
